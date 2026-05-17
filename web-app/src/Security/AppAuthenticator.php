ab3ff4be8ef162912ce9b32926c4b8ee94fc687f
enochjohn97 <ejidoko75@gmail.com>
2026-05-17 03:50:15 -0800

    Refactor user authentication to replace UserLoadingBadge with UserBadge for improved clarity

--- web-app/src/Security/AppAuthenticator.php
+++ web-app/src/Security/AppAuthenticator.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserLoadingBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
@@ -47,7 +47,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         return new Passport(
-            new UserLoadingBadge('username', $username),
+            new UserBadge($username),
             new PasswordCredentials($password),
             [
                 new CsrfTokenBadge('authenticate', $csrfToken),
